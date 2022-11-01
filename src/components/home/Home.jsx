e1a85622cf4332c194b86e53e13a71fc8c5d37e2
EliasComan <comanelias5@gmail.com>
2022-11-01 16:34:42 -0300

    styling home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,56 +1,63 @@
 import './styles.css'
 
-import { Link } from 'react-router-dom';
-import Marcas from '../marcas/Marcas';
-import React, {  } from "react";
+import { Link } from 'react-router-dom'
+import Marcas from '../marcas/Marcas'
+import React from 'react'
+import imageContacto from '../../images/ADMINISTRACION Y VENTAS 1.jpg'
+import imageEstampados from '../../images/MUESTRAS 5.jpg'
+import imageNosotros from '../../images/PERSONAL FABRICA 1.jpg'
+import imageSimulador from '../../images/IMG_3118.jpg'
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className='bannerInicio w-screen h-screen'>
-        <div className=' flex flex-col h-screen items-center justify-center'>
-            <h1 className='texto text-6xl text-white'>Somos fernatex</h1>
-            <div className='w-72 h-0.5 bg-slate-200'></div>
-            <h2 className='mt-2'>Pruebe nuestro simulador haciendo click <Link to={'/simulador/11047Aviones01'}><strong className=''>aqui</strong></Link> </h2>
+      <div className="bannerInicio w-screen h-screen">
+        <div className=" flex flex-col h-screen items-center justify-center">
+          <h1 className="texto text-6xl text-white">Somos fernatex</h1>
+          <div className="w-72 h-0.5 bg-slate-200"></div>
+          <h2 className="mt-2">
+            Pruebe nuestro simulador haciendo click{' '}
+            <Link to={'/simulador/11047Aviones01'}>
+              <strong className="">aqui</strong>
+            </Link>{' '}
+          </h2>
         </div>
-
       </div>
-      <div className=' my-24 bg-gray-100 w-5/6  border  rounded-xl'>
-    <div className=" flex flex-col items-center mt-12 align-center">
-        <h1 className="text-3xl  text-black font-semibold"> Aprenda mas acerca de nosotros.</h1>
-        <div className="bg-primary/80 shadow-2xl shadow-secondary/50 w-72 h-0.5 mt-2"></div>
+      <div className=" my-24  grid grid-rows-4 grid-cols-2 w-5/6 border rounded ">
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className='text-black text-3xl font-serif text-center'>Aprenda mas acerca<hr></hr> de <strong><Link to={'/nosotros'}>nosotros.</Link></strong></h1>
+          <h2 className='text-center text-black'>Haciendo click <strong><Link to={'/nosotros'}>aqui.</Link></strong></h2>
+        </div>
+        <div>
+          <img src={imageNosotros} className='w-full h-auto' alt="Imagen nostros" />
+        </div>
+        <div>
+          <img src={imageEstampados} className='w-full h-auto' alt="Imagen Estampados" />
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-black text-3xl font-serif text-center'>Vea nuestra galeria<hr></hr> de <strong><Link to={'/estampados'}>estampados.</Link></strong></h1>
+          <h2 className='text-center text-black'>Haciendo click <strong><Link to={'/estampados'}>aqui.</Link></strong></h2>
+        </div>
+        <div className='flex flex-col items-center justify-center '>
+        <h1 className='text-black text-3xl font-serif text-center'>Comuniquese con nosotros<hr></hr> en nuestra seccion de <strong><Link to={'/contacto'}>contacto.</Link></strong></h1>
+          <h2 className='text-center text-black'>Haciendo click <strong><Link to={'/contacto'}>aqui.</Link></strong></h2>
+       </div>
+       <div>
+        <img src={imageContacto}  className='w-full h-auto mt-24'alt="Imagen Contacto" />
+       </div>
+       <div>
+        <img src={imageSimulador} className='w-full h-auto mt-16' alt='IMagen simulador' />
+       </div>
+       <div className='flex flex-col items-center justify-center '>
+       <h1 className='text-black text-3xl font-serif text-center'>Ponga a prueba nuestros diseños<hr></hr> en nuestro  <strong><Link to={'/simulador/11047Aviones01'}>simulador.</Link></strong></h1>
+          <h2 className='text-center text-black'>Haciendo click <strong><Link to={'/simulador/11047Aviones01'}>aqui.</Link></strong></h2>
+        
+        </div>
+        
+      </div>
+      <Marcas />
     </div>
-      <div className="grid grid-cols-4 mt-4   border-t  gap-4 px-12   ">
-      <Link  to={'/nosotros'}  className="card grid  w-full h-72 border  shadow-xl  my-12">
-        <div className='flex flex-col items-center justify-center ' >
-            <h1 className='text-4xl'>Nosotros</h1>
-           <div className=' flex items-center  h-0.5 bg-white w-32 '></div>
-          </div>
-        </Link>
-        <Link to={'/estampados'}  className="card grid border w-full h-72 shadow-xl  my-12">
-        <div className='flex flex-col items-center justify-center ' >
-            <h1 className='text-4xl'>Estampados</h1>
-           <div className=' flex items-center  h-0.5 bg-white w-36 '></div>
-          </div>
-        </Link>
-        <Link to={'/contacto'}  className="card grid  w-full border h-72 shadow-xl  my-12">
-        <div className='flex flex-col items-center justify-center ' >
-            <h1 className='text-4xl'>Contacto</h1>
-           <div className=' flex items-center  h-0.5 bg-white w-32 '></div>
-          </div>
-        </Link>
-        <Link to={'/simulador/11047Aviones01'}  className="card border w-full h-72 grid  shadow-xl  my-12">
-        <div className='flex flex-col items-center justify-center ' >
-            <h1 className='text-4xl'>Simulador</h1>
-           <div className=' flex items-center  h-0.5 bg-white w-32 '></div>
-          </div>
-        </Link>
-      </div>
-      </div>
-      <Marcas/>
-     
-    </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- src/components/marcas/Marcas.jsx
+++ src/components/marcas/Marcas.jsx
@@ -5,13 +5,13 @@ import { motion } from 'framer-motion'
 const Marcas = () => {
 const render = MarcasData.map((item, index )=> (
     <motion.div key={item.nombre+index} className='flex flex-col items-center justify-center'>
-        <motion.img src={item.imagen} className='w-20 rounded-full' ></motion.img>
+        <motion.img src={item.imagen} className='w-20 rounded-full shadow-xl' ></motion.img>
         {item.nombre}
     </motion.div>
 )
   )
     return (
-    <div className='flex items-center flex-col w-full'>
+    <div className='flex items-center flex-col w-full border-t border-dotted border-spacing-9 pt-2'>
         <div className='flex flex-col items-center justify-center'>
             <h1 className='text-3xl  font-semibold text-black'>Marcas con las que trabajamos</h1>
             <div className="bg-primary/80 shadow-2xl shadow-secondary/50 w-96 h-0.5 mt-2"></div>

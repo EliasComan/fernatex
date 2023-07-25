70a67dcfeb790be23e7ea3d481a13612d0e8f17c
eliasComan <comanelias5@gmail.com>
2023-07-25 03:30:24 -0300

    nosotros mobile

--- src/components/contacto/index.js
+++ src/components/contacto/index.js
@@ -5,8 +5,8 @@ const Contacto = () => {
   return (
     <div className='flex itemas-center justify-center mt-12 '>
       <div className=''>
-        <div className="flex flex-col h-screen w-screen items-center ">
-          <div className=' h-2/4 w-screen'>
+        <div className="flex flex-col md:flex-row h-screen w-screen items-center md:px-20">
+          <div className=' h-2/4 w-screen md:w-2/4'>
           <iframe title='MAPA' 
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.579620110964!2d-58.62801343748407!3d-34.6653180285921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc740fee42409%3A0xfa735e1059054c0f!2sSta.%20Mar%C3%ADa%20265%2C%20B1708%20Mor%C3%B3n%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1689627221544!5m2!1ses!2sar" 
           loading='lazy'
@@ -15,24 +15,31 @@ const Contacto = () => {
           className='border rounded-md'
           referrerPolicy="no-referrer-when-downgrade"/>
           </div>
-          <div className=' flex bg-gray-100 flex-col w-screen h-2/4 justify-around items-center w-5/6 my-12 border-lg rounded-xl shadow z-10 '>
+          <div className=' flex flex-col w-screen md:w-2/4 h-2/4 justify-around items-center  w-5/6 my-12 border-lg rounded-xl shadow z-10 '>
            
-              <h2 className='text-black block font-semibold text-xl text-center '>Comuniquese con nosotros</h2>
-              <div className='flex flex-col items-start justify-center'>
-                <label className='text-black text-md'>Nombre completo</label>
-                <input type='text'  className='w-72 border rounded-md shadow-md'/>
-              </div>
-              <div className='flex flex-col items-start justify-center'>
-                <label className='text-black text-md '>Email.</label>
-                <input type='text'  className='w-72 border rounded-md shadow-md'/>
-              </div>
-              <div className='flex flex-col items-staRT justify-center'>
-                <label className='text-black text-md'>Numero de telefono</label>
-                <input type='text'  className='w-72 border rounded-md shadow-md'/>
-              </div>
-              <div className='w-screen flex items-center justify-center'>
+              <h2 className='text-black block font-semibold text-2xl text-center md:text-4xl  '>Comuniquese con nosotros</h2>
+             <div className='flex md:w-full flex-col md:flex-row justify-center md:justify-around items-center'>
+                <div className='flex flex-col items-start justify-center'>
+                  <label className='text-black text-md'>Nombre completo.</label>
+                  <input type='text'  className='w-72 border rounded-md shadow-md'/>
+                </div>
+                <div className='flex flex-col items-start justify-center'>
+                  <label className='text-black text-md '>Email.</label>
+                  <input type='text'  className='w-72 border rounded-md shadow-md'/>
+                </div>
+            </div>
 
-              <motion.button whileHover={{scale:1.2}} onHoverStart={e => {}} onHoverEnd={e => {}}   whileTap={{ scale: 0.9 }} className='w-2/4 bg-primary h-12 border rounded-xl text-white shadow-md' type='button' >Enviar</motion.button>
+              <div className='flex flex-col items-start md:w-full  justify-center md:px-20 '>
+                <label className='text-black text-md'>Numero de telefono.</label>
+                <input type='text'  className='w-72 md:w-content md:px-24  border  rounded-md shadow-md'/>
+              </div>
+              <div className='flex flex-col items-start justify-center md:w-full md:justify-start md:px-20'>
+                <label className='text-black'>Deje un comentario</label>
+                <textarea className='border rounded-xl w-72 md:w-full h-24 px-24 shadow-md'></textarea>
+              </div>
+              <div className='w-screen md:w-full flex items-center justify-center'>
+
+              <motion.button whileHover={{scale:1.2}} onHoverStart={e => {}} onHoverEnd={e => {}}   whileTap={{ scale: 0.9 }} className='w-2/4 my-8 md:my-0 md:w-72 bg-primary h-12 border rounded-xl text-white shadow-md' type='button' >Enviar</motion.button>
             </div>
 
 
--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -10,7 +10,7 @@ import { motion } from 'framer-motion'
 const Nosotros = () => {
   return (
     <div >
-        <div className='banner w-full h-72 flex items-center justify-center'>
+        <div className='banner w-screen h-72 flex items-center justify-center'>
            <h1 className='text-7xl text-white'>Nosotros</h1>
         </div>
         <motion.div initial={{ opacity: 0 }}
@@ -18,18 +18,17 @@ const Nosotros = () => {
             transition={{ duration: 1 }}
             viewport={{ once: true }}
             exit={{ opacity: 0 }} layout 
-            className='flex flex-col w-full items-center justify-center'>
+            className='flex flex-col w-screen items-center justify-center'>
             <div className='flex my-24 flex flex-col-reverse md:flex-row'>
                 <div className=' mt-20'>
-                <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}}   className='w-96 h-auto' src={imgEdificio} alt="" />
-                <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}}  className='w-96   h-auto mt-4' src={imgFabrica} alt=''/>
+                <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}}   className='w-screen md:w-96 h-auto' src={imgEdificio} alt="" />
+                <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}}  className='w-screen md:w-96   h-auto mt-4' src={imgFabrica} alt=''/>
 
                 </div>
                 <div className='flex flex-col  justify-center '>
                     <h1 className='text-4xl font-bold font-sans mx-12 text-black'>Fernatex</h1>
-                    <div className='bg-primary flex items-center h-0.5 w-36 mx-12'></div>
-                    <p className='text-ellipsis text-xl w-96 mx-12 my-4 text-black'>La Empresa se funda en Septiembre de 1973, en manos de la familia Fernández. Su actividad, se avocaba  a la estampa artesanal con estampados manuales y automáticos.</p>
-                     <p className='text-ellipsis w-96 text-xl mx-12 my-4 text-black'> En Marzo del 2008, la empresa se transforma. Adquirida  por la Familia Pazos le inyectan un nuevo aire comercial quienes incorporan nuevas  máquinas  para la producción de nuevos estampados en todos los rubros. </p>
+                    <p className='text-ellipsis text-xl text-center w-96 md:mx-12 my-4 text-black'>La Empresa se funda en Septiembre de 1973, en manos de la familia Fernández. Su actividad, se avocaba  a la estampa artesanal con estampados manuales y automáticos.</p>
+                     <p className='text-ellipsis w-96 text-xl text-center md:mx-12 my-4 text-black'> En Marzo del 2008, la empresa se transforma. Adquirida  por la Familia Pazos le inyectan un nuevo aire comercial quienes incorporan nuevas  máquinas  para la producción de nuevos estampados en todos los rubros. </p>
                        
                 </div>
             </div>
@@ -42,18 +41,17 @@ const Nosotros = () => {
          transition={{ duration: 1 }}
          viewport={{ once: true }}
          exit={{ opacity: 0 }}
-         className='flex  flex-col w-full items-center justify-center'>
+         className='flex  flex-col w-screen items-center justify-center'>
             <div className='flex flex-col md:flex-row my-24'>
                 <div className='flex flex-col justify-center '>
                     <h1 className='text-4xl font-bold font-sans mx-12 text-black'>Nuestra historia</h1>
-                    <div className='bg-primary h-0.5 w-48 mx-12'></div>
-                    <p className='text-ellipsis text-xl w-96 mx-12 my-4 text-black'>La empresa, ubicada en Morón Provincia de Buenos Aires, cuenta con un gran grupo de personas que  trabaja para ampliar cada día nuestro campo de tendencias. Nuestro equipo de ventas y diseño trabajan en conjunto, apoyados por el Sector de Muestras y Producción, para brindar a nuestros clientes el mejor servicio personalizado adaptado a sus necesidades, haciendo que sus productos sean UNICOS</p>
+                    <p className='text-ellipsis text-xl w-96 md:mx-12 my-4 text-black text-center'>La empresa, ubicada en Morón Provincia de Buenos Aires, cuenta con un gran grupo de personas que  trabaja para ampliar cada día nuestro campo de tendencias. Nuestro equipo de ventas y diseño trabajan en conjunto, apoyados por el Sector de Muestras y Producción, para brindar a nuestros clientes el mejor servicio personalizado adaptado a sus necesidades, haciendo que sus productos sean UNICOS</p>
                     
                       
                 </div>
                 <div className=' mt-20'>
-                    <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}} className='w-96 h-auto' src={adminyventas} alt="" />
-                    <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}} className='w-96   h-auto mt-4' src={imgPersonal} alt=''/>
+                    <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}} className=' w-screen md:w-96  h-auto' src={adminyventas} alt="" />
+                    <motion.img layout initial={{opacity:0}}whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}} className='w-screen md:w-96    h-auto mt-4' src={imgPersonal} alt=''/>
 
                 </div>
             </div>
